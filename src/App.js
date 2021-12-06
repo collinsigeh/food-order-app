7ba3a970b9781dd789c2c18ce8f8a3ada148b515
Collins Igeh <igehac@gmail.com>
2021-12-06 13:44:41 +0100

    implemented the meals component with sub meals summary and available meals components. Also created a generic Card UI component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Header from "./modules/Layout/Header";
+import Meals from "./modules/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <Meals />
     </Fragment>
   );
 }
