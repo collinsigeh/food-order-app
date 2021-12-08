e97dcb51d0b7b31fef3424780a875428b04323ec
Collins Igeh <igehac@gmail.com>
2021-12-08 11:28:28 +0100

    implemented a ported modal compoenent for display the cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Header from "./modules/Layout/Header";
 import Meals from "./modules/Meals/Meals";
+import Cart from "./modules/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <Meals />
     </Fragment>
